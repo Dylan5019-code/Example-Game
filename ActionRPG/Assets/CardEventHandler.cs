5b231d23ead954b7f303b94a0d2485decea428da
Dylan5019 <71289446+Dylan5019@users.noreply.github.com>
2021-03-31 18:52:29 -0500

    Add Current Project Setup

---
+++ ActionRPG/Assets/CardEventHandler.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardEventHandler : MonoBehaviour
+{
+    public GameObject UiObject;
+    public GameObject UiObject1;
+    public GameObject UiObject2;
+    public GameObject enemy; 
+    public int computerChoice;
+
+
+    private void Start()
+    {
+        computerChoice = Random.Range(1, 4);
+    }
+
+
+    public void pickRock()
+    {
+        Debug.Log("You Choose Rock");
+        chooseAction(1);
+        //hideUi();
+
+    }
+
+    public void pickPaper()
+    {
+        Debug.Log("You Choose Paper");
+        chooseAction(2);
+        //hideUi();
+    }
+
+    public void pickScissors()
+    {
+        Debug.Log("You Choose Scissors");
+        chooseAction(3);
+        //hideUi();
+    }
+
+    void chooseAction(int choice)
+    {
+        
+        if (choice == 1)
+        {
+            UiObject.SetActive(false);
+            Destroy(enemy);
+        }
+        else if (choice == 2)
+        {
+            UiObject1.SetActive(false);
+            Destroy(enemy);
+        }
+        else if (choice == 3)
+        {
+            UiObject2.SetActive(false);
+            Destroy(enemy);
+        }
+        else
+        {
+            Debug.Log("Error");
+        }
+
+    }
+
+
+}
---
+++ ActionRPG/Assets/ShowGUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowGUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject UiObject;
+    public GameObject UiObject1;
+    public GameObject UiObject2;
+
+
+    private void Start()
+    {
+        UiObject.SetActive(false);
+        UiObject1.SetActive(false);
+        UiObject2.SetActive(false);
+    }
+
+
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag== "Player")
+        {
+            UiObject.SetActive(true);
+            UiObject1.SetActive(true);
+            UiObject2.SetActive(true);
+        }    
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        UiObject.SetActive(false);
+        UiObject1.SetActive(false);
+        UiObject2.SetActive(false);
+    }
+
+}
